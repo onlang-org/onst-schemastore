ffbdbb3fc6296b7488a5fa0e4db03d8c234b29e4
rajatasusual <krajat4@gmail.com>
2024-03-09 21:06:43 +0000

    v1.2.1 exporting fetchSchemaFromSchemaStore from main module

--- main.js
+++ main.js
@@ -122,8 +122,44 @@ function findSchema(input, categorizedSchemas) {
     return closestMatches;
 }
 
+/**
+ * Fetches the schema from the schema store using autocomplete to select an entity.
+ *
+ * @return {Object} The selected schema object.
+ */
+async function fetchSchemaFromSchemaStore() {
+
+    const { default: autocomplete } = await import(
+        'inquirer-autocomplete-standalone'
+    );
+
+    const categorizedSchemas = await categorizeSchemas();
+
+    const answer = await autocomplete({
+        message: 'Which entity would you want to download?',
+        source: async (input) => {
+            const filteredEntities = await findSchema(input, categorizedSchemas)
+            return filteredEntities.map(schema => {
+                return {
+                    value: schema,
+                    description: `${schema} selected`
+                }
+            })
+        }
+    });
+
+    const result = categorizedSchemas.find(schema => schema.name === answer);
+
+    console.log(`${answer} has ${result.schema.length} ${result.schema.length === 1 ? 'schema' : 'schemata'}.`);
+
+    result.schema.forEach(schema => {
+        console.log(`- ${schema.name} - ${schema.url}`);
+    })
+
+}
 
 module.exports = {
     categorizeSchemas,
-    findSchema
+    findSchema,
+    fetchSchemaFromSchemaStore
 }
